71b79c651f35f52483d5dccd0797ae03baff5534
duemaster <zhangbsn@gmail.com>
2018-04-10 11:07:52 +0800

    Update Setup

--- composer-playground/setup.sh
+++ composer-playground/setup.sh
@@ -31,13 +31,16 @@ docker exec cli composer network deploy --archiveFile /mnt/air-chain.bna -A admi
 docker exec cli composer card import -f admin1@air-chain.card;
 
 echo "Creating default Airline Company: Airline1"
-docker exec cli composer transaction submit --card admin1@air-chain -d '{"$class": "org.hyperledger.composer.system.AddAsset","registryType": "Asset","registryId": "org.airline.airChain.AirlineCompany", "targetRegistry" : "resource:org.hyperledger.composer.system.AssetRegistry#org.airline.airChain.AirlineCompany", "resources": [{"$class": "org.airline.airChain.AirlineCompany","id": "Airline1","name": "MyAirline1"}]}';
+docker exec cli composer transaction submit --card admin1@air-chain -d '{"$class": "org.hyperledger.composer.system.AddAsset","registryType": "Asset","registryId": "org.airline.airChain.AirlineCompany", "targetRegistry" : "resource:org.hyperledger.composer.system.AssetRegistry#org.airline.airChain.AirlineCompany", "resources": [{"$class": "org.airline.airChain.AirlineCompany","id": "Airline1","name": "Singapore Airlines", "authorisedCargoCompany": "Cargo1"}]}';
 
 echo "Creating default Cargo Company: Cargo1"
-docker exec cli composer transaction submit --card admin1@air-chain -d '{"$class": "org.hyperledger.composer.system.AddAsset","registryType": "Asset","registryId": "org.airline.airChain.CargoCompany", "targetRegistry" : "resource:org.hyperledger.composer.system.AssetRegistry#org.airline.airChain.CargoCompany", "resources": [{"$class": "org.airline.airChain.CargoCompany","id": "Cargo1","name": "MyCargo1"}]}';
+docker exec cli composer transaction submit --card admin1@air-chain -d '{"$class": "org.hyperledger.composer.system.AddAsset","registryType": "Asset","registryId": "org.airline.airChain.CargoCompany", "targetRegistry" : "resource:org.hyperledger.composer.system.AssetRegistry#org.airline.airChain.CargoCompany", "resources": [{"$class": "org.airline.airChain.CargoCompany","id": "Cargo1","name": "Sing Air Cargo"}]}';
+
+echo "Creating default Cargo Company: Cargo2"
+docker exec cli composer transaction submit --card admin1@air-chain -d '{"$class": "org.hyperledger.composer.system.AddAsset","registryType": "Asset","registryId": "org.airline.airChain.CargoCompany", "targetRegistry" : "resource:org.hyperledger.composer.system.AssetRegistry#org.airline.airChain.CargoCompany", "resources": [{"$class": "org.airline.airChain.CargoCompany","id": "Cargo2","name": "Second Person Cargo"}]}';
 
 echo "Creating default GHA Company: GHA1"
-docker exec cli composer transaction submit --card admin1@air-chain -d '{"$class": "org.hyperledger.composer.system.AddAsset","registryType": "Asset","registryId": "org.airline.airChain.GHACompany", "targetRegistry" : "resource:org.hyperledger.composer.system.AssetRegistry#org.airline.airChain.GHACompany", "resources": [{"$class": "org.airline.airChain.GHACompany","id": "GHA1","name": "MyGHA1"}]}';
+docker exec cli composer transaction submit --card admin1@air-chain -d '{"$class": "org.hyperledger.composer.system.AddAsset","registryType": "Asset","registryId": "org.airline.airChain.GHACompany", "targetRegistry" : "resource:org.hyperledger.composer.system.AssetRegistry#org.airline.airChain.GHACompany", "resources": [{"$class": "org.airline.airChain.GHACompany","id": "GHA1","name": "SATS"}]}';
 
 echo "Start REST Server at Port: 3000"
 docker exec cli composer-rest-server -c admin1@air-chain -p 3000;
